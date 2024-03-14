7a349f02290fa9b712ed76ea5a4e2b3eff28b5a8
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-03-14 13:11:06 -0400

    Update docker run

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -9,10 +9,12 @@ docker run --name myga --detach \
 --publish 9021:8021 \
 --publish 9022:8022 \
 --publish 32001-32300:32001-32300 \
+--volume /home/azureuser/volumes/gamft.lic:/opt/HelpSystems/GoAnywhere/gamft.lic \
 --volume userdatadir:/opt/HelpSystems/GoAnywhere/userdata/ \
---volume upgraderdir:/opt/HelpSystems/GoAnywhere/upgrader/ \
+--volume ${HOME}/volumes/:/opt/HelpSystems/GoAnywhere/upgrader/ \
 --volume configdir:/etc/HelpSystems/GoAnywhere/config/ \
 --volume tomcatserver:/etc/HelpSystems/GoAnywhere/tomcat/ \
---volume tomcatlogsdir:/opt/HelpSystems/GoAnywhere/tomcat/logs/ \
---volume ghttpsrootcustomdir:/opt/HelpSystems/GoAnywhere/ghttpsroot/custom/ \
+--volume ${HOME}/volumes/:/opt/HelpSystems/GoAnywhere/tomcat/logs/ \
+--volume ${HOME}/volumes/:/opt/HelpSystems/GoAnywhere/ghttpsroot/custom/ \
+--mac-address "02:42:c0:a8:80:80" \
 myga:latest
