ab49a228e0829a284a007db8f66218538e8f017b
cedric guindon <cedric.guindon@gmail.com>
2024-03-22 10:13:32 -0400

    Add config and db logic

--- README.md
+++ README.md
@@ -2,7 +2,36 @@
 This project is a test to for the GoAnywhere container
 
 # Container Image Build
-Run the following commands from the project root directory:
+Run the following commands from {project root directory}/git/ga:
 ```
 docker build -t myga:latest .
 ```
+
+# Run Instructions
+## Environment Variables
+
+The following environment variables are used to control the application at run-time. Mandatory variables are marked with an asterisk.
+
+> **MFT_CLUSTER**: Enables cluster for the application.
+- Allowed values: TRUE or FALSE
+- Default value: FALSE
+
+> **SYSTEM_NAME**: The name for the application used in the cluster.
+- Default value: none
+
+> **DB_PASSWORD**: The password used by the application to connect to the database server.
+- Default value: none
+
+> **DB_USERNAME**: The username used by the application to connect to the database server.
+- Default value: none
+
+> **DB_DRIVERCLASSNAME**: The driver class name used by the application to connect to the database server.
+- Default value: none
+- Allowed values: 
+    - mariaDB:org.mariadb.jdbc.Driver
+
+> **DB_URL**: The connection string to connect to the database server.
+- Default value: none
+
+> JAVA_MAX_MEMORY: The java memory required for the application.
+- Default value: 1024
--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -16,13 +16,14 @@ echo "********************************************"
 echo "********************************************"
 echo "********************************************"
 
-# Verify files are owned by gamft:root
+# Verify files are owned by gamft:root.
 echo "Updating files permissions for /etc/HelpSystems/GoAnywhere and /opt/HelpSystems/GoAnywhere directories..."
 sudo find /etc/HelpSystems/GoAnywhere \! -user gamft -exec chown gamft:root {} \;
 sudo find /opt/HelpSystems/GoAnywhere \! -user gamft -exec chown gamft:root {} \;
 
 cd /opt/HelpSystems/GoAnywhere
 
+# Update the file: cluster.xml with the host values.
 if [ "$MFT_CLUSTER" == "TRUE" ]; then
   host=`hostname -i`
   sed -i "s|systemName\">.*<|systemName\">$SYSTEM_NAME<|g" /etc/HelpSystems/GoAnywhere/config/cluster.xml
@@ -30,6 +31,14 @@ if [ "$MFT_CLUSTER" == "TRUE" ]; then
   sed -i 's|clusterBindPort"><|clusterBindPort">8006<|g' /etc/HelpSystems/GoAnywhere/config/cluster.xml
   sed -i 's|false|true|g' /etc/HelpSystems/GoAnywhere/config/cluster.xml
 fi
+
+# Update the file database.xml with the correct values.
+sed -i "s|sequencePoolMaxIdle\">.*<|sequencePoolMaxIdle\">5<|g" /etc/HelpSystems/GoAnywhere/config/database.xml
+sed -i "s|password\">.*<|password\">$DB_PASSWORD<|g" /etc/HelpSystems/GoAnywhere/config/database.xml
+sed -i "s|username\">.*<|username\">$DB_USERNAME<|g" /etc/HelpSystems/GoAnywhere/config/database.xml
+sed -i "s|driverClassName\">.*<|driverClassName\">$DB_DRIVERCLASSNAME<|g" /etc/HelpSystems/GoAnywhere/config/database.xml
+sed -i "s|url\">.*<|url\">$DB_URL<|g" /etc/HelpSystems/GoAnywhere/config/database.xml
+sed -i "s|</properties>|<entry key=\"passwordIsEncrypted\">true</entry>\r</properties>|g" /etc/HelpSystems/GoAnywhere/config/database.xml
 
 JVM='1024'
 if [ -n "$JAVA_MAX_MEMORY" ]; then
@@ -41,7 +50,7 @@ export JAVA_OPTS
 
 PRGDIR=`pwd`
 
-# Use the bundled JRE if one has been bundled
+# Use the bundled JRE if one has been bundled.
 if [ -d "$PRGDIR/jre6" ]
 then
   export JAVA_HOME="$PRGDIR/jre6"
