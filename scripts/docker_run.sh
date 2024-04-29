216a21f6fa62a7f0e1561160da1a87c9d84e520c
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-04-29 15:38:14 -0400

    push new branch

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -1,20 +1,19 @@
-docker run --name myga --detach \
---publish 9000:8000 \
---publish 9001:8001 \
---publish 9005:8005 \
---publish 9006:8006 \
---publish 9009:8009 \
---publish 9010:8010 \
---publish 9443:8443 \
---publish 9021:8021 \
---publish 9022:8022 \
+docker run --name ga --detach \
+--publish 8000:8000 \
+--publish 8001:8001 \
+--publish 8005:8005 \
+--publish 8006:8006 \
+--publish 8009:8009 \
+--publish 8010:8010 \
+--publish 8443:8443 \
+--publish 8021:8021 \
+--publish 8022:8022 \
 --publish 32001-32300:32001-32300 \
---volume /home/azureuser/volumes/gamft.lic:/opt/HelpSystems/GoAnywhere/gamft.lic \
---volume userdatadir:/opt/HelpSystems/GoAnywhere/userdata/ \
---volume ${HOME}/volumes/:/opt/HelpSystems/GoAnywhere/upgrader/ \
---volume configdir:/etc/HelpSystems/GoAnywhere/config/ \
---volume tomcatserver:/etc/HelpSystems/GoAnywhere/tomcat/ \
---volume ${HOME}/volumes/:/opt/HelpSystems/GoAnywhere/tomcat/logs/ \
---volume ${HOME}/volumes/:/opt/HelpSystems/GoAnywhere/ghttpsroot/custom/ \
---mac-address "02:42:c0:a8:80:80" \
-myga:latest
+--volume /home/ec2-user/efs/userdatadir:/opt/HelpSystems/GoAnywhere/userdata/ \
+--volume /home/ec2-user/efs/upgrader:/opt/HelpSystems/GoAnywhere/upgrader/ \
+--volume /home/ec2-user/efs/configdir:/etc/HelpSystems/GoAnywhere/config/ \
+--volume /home/ec2-user/efs/tomcatserver:/etc/HelpSystems/GoAnywhere/tomcat/ \
+--volume /home/ec2-user/efs/tomcat:/opt/HelpSystems/GoAnywhere/tomcat/logs/ \
+--volume /home/ec2-user/efs/ghttpsroot:/opt/HelpSystems/GoAnywhere/ghttpsroot/custom/ \
+--volume /home/ec2-user/efs/sharedconfig:/etc/HelpSystems/GoAnywhere/sharedconfig/ \
+helpsystems/goanywhere-mft:latest
