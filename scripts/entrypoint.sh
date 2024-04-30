ffe3ea9845f65beba1be51bf56453a91eadec048
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-04-30 13:27:46 -0400

    test

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -42,20 +42,7 @@ ls -la /home/volumes/sharedconfig/
 shareconfig_folder="/etc/HelpSystems/GoAnywhere/sharedconfig"
 
 
-cd "${config_folder}"
-shopt -s extglob
-rm -- !("cluster.xml")
-ln -s "${shareconfig_folder}"/database.xml "${config_folder}"/database.xml
-ln -s "${shareconfig_folder}"/agent.xml "${config_folder}"/agent.xml
-ln -s "${shareconfig_folder}"/ftp.xml "${config_folder}"/ftp.xml
-ln -s "${shareconfig_folder}"/ftps.xml "${config_folder}"/ftps.xml
-ln -s "${shareconfig_folder}"/gateway.xml "${config_folder}"/gateway.xml
-ln -s "${shareconfig_folder}"/gofast.xml "${config_folder}"/gofast.xml
-ln -s "${shareconfig_folder}"/https.xml "${config_folder}"/https.xml
-ln -s "${shareconfig_folder}"/log4j2.xml "${config_folder}"/log4j2.xml
-ln -s "${shareconfig_folder}"/pesit.xml "${config_folder}"/pesit.xml
-ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
-ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
+
 
 echo ""
 echo ""
