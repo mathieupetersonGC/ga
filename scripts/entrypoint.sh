2c4dde423cc5128b862e4b610f155586a59ed03b
Mathieu Peterson <mathieu.peterson@bac-lac.gc.ca>
2024-04-30 13:49:13 -0400

    sss

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -39,38 +39,28 @@ echo Listing /home/volumes/sharedconfig/...
 echo "********************************************"
 ls -la /home/volumes/sharedconfig/
 
-# variables.
-shareconfig_folder="/etc/HelpSystems/GoAnywhere/sharedconfig"
 
-
-cd "${config_folder}"
-#shopt -s extglob
-#rm -- !("cluster.xml")
-ln -s "${shareconfig_folder}"/database.xml "${config_folder}"/database.xml
-ln -s "${shareconfig_folder}"/agent.xml "${config_folder}"/agent.xml
-ln -s "${shareconfig_folder}"/ftp.xml "${config_folder}"/ftp.xml
-ln -s "${shareconfig_folder}"/ftps.xml "${config_folder}"/ftps.xml
-ln -s "${shareconfig_folder}"/gateway.xml "${config_folder}"/gateway.xml
-ln -s "${shareconfig_folder}"/gofast.xml "${config_folder}"/gofast.xml
-ln -s "${shareconfig_folder}"/https.xml "${config_folder}"/https.xml
-ln -s "${shareconfig_folder}"/log4j2.xml "${config_folder}"/log4j2.xml
-ln -s "${shareconfig_folder}"/pesit.xml "${config_folder}"/pesit.xml
-ln -s "${shareconfig_folder}"/security.xml "${config_folder}"/security.xml
-ln -s "${shareconfig_folder}"/sftp.xml "${config_folder}"/sftp.xml
 
 echo ""
 echo ""
 echo ""
 
 echo "********************************************"
-echo Listing "${config_folder}"...
+echo Listing "/etc/HelpSystems/GoAnywhere/config"...
 echo "********************************************"
 
-ls -la "${config_folder}"
-
+ls -la /etc/HelpSystems/GoAnywhere/config/
 
 echo "********************************************"
-echo cat ${config_folder}/database.xml...
+echo Listing "/opt/HelpSystems/GoAnywhere/userdata/"...
 echo "********************************************"
-cat ${config_folder}/database.xml
+
+ls -la /opt/HelpSystems/GoAnywhere/userdata/
+
+echo "********************************************"
+echo Listing "/etc/HelpSystems/GoAnywhere/tomcat/"...
+echo "********************************************"
+
+ls -la /etc/HelpSystems/GoAnywhere/tomcat/
+
 
